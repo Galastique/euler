51a0a5c9ae972f0f2a5b8c12e824335808f42dc5
Galastique <anthokess@hotmail.com>
2021-10-18 16:46:52 -0400

    Add files via upload

--- euler/problem4.py
+++ euler/problem4.py
@@ -2,43 +2,46 @@
 
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
+# https://stackoverflow.com/questions/6037973/project-euler-problem-number-4
 
 #List
 palindromes = []
-test = []
 
 #Variables
 maxX = 999
 maxY = 999
 number = 998001
-x = 0
-y = 0
 palindrome = number
 
-#Finds all palindromes
-while palindrome >= 100000:
-
-    #Converts palindrome to string
+#Function that checks for palindromes
+def checkPalindome():
+    #Convert number into string
     checkPalindrome = str(palindrome)
-
-    #Checks if number is palindrome
-    if (checkPalindrome[0] == checkPalindrome[-1]) and (checkPalindrome[1] == checkPalindrome[-2]) and (checkPalindrome[2] == checkPalindrome[-3]):
-            
+    #Checks palindromes
+    if (checkPalindrome[0] == checkPalindrome[-1]) and (checkPalindrome[1] == checkPalindrome[-2]) and (checkPalindrome[2] == checkPalindrome[-3]): 
         #Adds palindrome to list
         palindromes.append(palindrome)
-        
-        """
-        #Checks palindromes for multipliers
-        for x in palindromes:
-            if palindrome % maxX == 0:
-                test.append(maxX)
-            if maxX > 1:
-                maxX -= 1
-        """
-        
+
+#Finds all palindromes between 100x100 and 999x999
+for x in range(100000, number):            
+
+    #Checks palindromes for multipliers
+    a = 100
+    b = 100
+    biggest = 0
+    for a in range(a, 999):
+        for b in range(b, 999):
+            product = a * b
+            if checkPalindome:
+                biggest = product
+            #b += 1
+        b = 100
+        #a += 1
+    
     #Finds more palindromes
-    palindrome -= 1
+    #palindrome -= 1
 
 #Displays answers
 print("\n")
 print(palindromes)
+print(biggest)

c99021722970dbad9bc7b13d3cb3aa3d15ae1d53
Galastique <anthokess@hotmail.com>
2021-10-28 16:36:45 -0400

    Add files via upload

--- euler/problem14.py
+++ euler/problem14.py
@@ -1,16 +1,32 @@
 #Variables
 n = 0
-temp = 0
+biggest = 0 #biggest chain
+number = 0 #number that produces biggest chain
+answer = 0
 
-#
-for n in range(0, 1000000):
+#Loop
+for n in range(1, 1000000, 2):
+    temp = 0
+    number = n
 
-    #if number is even
-    if n % 2 == 0:
-        n = n/2
-        temp+=1
+    #your mom
+    while n>1:
 
-    #if number is odd
-    else:
-        n = 3*n+1
-        temp+=1
+        #if number is even
+        if n % 2 == 0:
+            n = n/2
+            temp+=1
+
+        #if number is odd
+        else:
+            n = 3*n+1
+            temp+=1
+        
+        #Checks if new chain is bigger
+        if temp > biggest:
+            biggest = temp
+            answer = number
+
+
+#Shows answers
+print(answer)

1d86ad41377993ed0afc5090ac3139b7529c8fdd
Galastique <anthokess@hotmail.com>
2021-11-03 12:57:01 -0400

    Add files via upload

--- euler/problem12.py
+++ euler/problem12.py
@@ -1,24 +1,24 @@
 #Variables
 answer = 0
-divisors = 0
-
+divisors = 0 #amount of divisors a number has
+number = 0 #triangular number
 
 #Loop
-for x in range(1, 1000000):
+for x in range(1,10000000):
     divisors = 0
+    number+=x #+1, +2, +3, +4...
 
     #Adds numbers to list
-    for y in range (1, x+1):
-        if x % y == 0:
+    for y in range(1,number+1):
+        if number % y == 0:
             divisors+=1
 
-
     #Checks if answer has been found
-    if divisors > 500:
-        answer = x
-        print(answer)
+    if divisors > 4:
+        answer = number
         break
 
 
 #Displays answers
-print(answer)
+print(x) #2031455
+print(answer) #682512600 (wrong)

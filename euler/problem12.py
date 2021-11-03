3bc8f1918e7acaddfc73c33ea20a13206d815816
Galastique <anthokess@hotmail.com>
2021-11-03 13:00:08 -0400

    Add files via upload

--- euler/problem12.py
+++ euler/problem12.py
@@ -5,8 +5,8 @@ number = 0 #triangular number
 
 #Loop
 for x in range(1,10000000):
-    divisors = 0
-    number+=x #+1, +2, +3, +4...
+    divisors = 0 #resets amount
+    number+=x #gets new value of triangular number
 
     #Adds numbers to list
     for y in range(1,number+1):
@@ -14,7 +14,7 @@ for x in range(1,10000000):
             divisors+=1
 
     #Checks if answer has been found
-    if divisors > 4:
+    if divisors > 500:
         answer = number
         break
 

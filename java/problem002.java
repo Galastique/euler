854fff27a389cca523340f4edaf1b10ad1a51d1e
Galastique <anthokess@hotmail.com>
2021-11-05 16:48:20 -0400

    Add files via upload

---
+++ java/problem002.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class problem002 {
+	public static void main(String[] args) {
+
+		// Variables
+		List<Integer> fibonacciNumbers = new ArrayList<>(0, 1);
+		int answer = 0;
+
+		// Executes for every number under 4000000 in intervals of 2
+		for (int i = 0; i < 4000000; i += 2) {
+
+			// Gets value of next number
+			int x = fibonacciNumbers.get(fibonacciNumbers.size() - 1) + fibonacciNumbers.get(fibonacciNumbers.size() - 2);
+
+			// Adds number to list
+			if(x < 4000000) {
+				fibonacciNumbers.add(x);
+
+				//Checks if number is even
+				if(x % 2 == 0){
+					answer+=x;
+				}
+			}
+
+		}
+
+		// Displays answer
+		System.out.print(answer);
+	}
+}
--- python/problem002.py
+++ python/problem002.py
@@ -1,10 +1,9 @@
 #Variables
 fibonacciNumbers = [0,1]
-evenFibonacciNumbers = []
-
+answer = 0
 
 #Loop
-for x in range(0,4000000,2):
+for i in range(0,4000000,2):
 
     #Finds value of next number
     x = fibonacciNumbers[-2] + fibonacciNumbers[-1]
@@ -13,17 +12,10 @@ for x in range(0,4000000,2):
     if x < 4000000:
         fibonacciNumbers.append(x)
 
+        #Adds even fibonacci number
+        if x % 2 == 0:
+            answer+=x
 
-#For every number in list
-for y in fibonacciNumbers:
-
-    #Finds even numbers from list
-    if y % 2 == 0:
-        evenFibonacciNumbers.append(y)
-
-
-#Gets answer
-answer = sum(evenFibonacciNumbers)
 
 #Displays answer
 print(answer)

7066d8ca5cb4b75c1a33829bb11430b0f6e10e62
Galastique <anthokess@hotmail.com>
2021-11-08 09:59:32 -0500

    Add files via upload

---
+++ javascript/problem001.js
@@ -0,0 +1,14 @@
+//Variables
+var answer = 0;
+
+//Finds the multiples of 3 or 5 under 1000
+for (x = 0; x < 1000; x++) {
+    
+    if (x % 3 == 0 || x % 5 == 0) {
+
+        answer += x;
+    }
+}
+
+//Displays answer
+console.log(answer)

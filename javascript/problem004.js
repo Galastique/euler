4f5a3a4e56cbf81afd3401fabefbbd174f06c48b
Galastique <anthokess@hotmail.com>
2021-11-09 13:29:40 -0500

    Add files via upload

--- javascript/problem004.js
+++ javascript/problem004.js
@@ -9,10 +9,12 @@ for (a = 100; a <= 999; a++) {
     //Checks second multiplier
     for (b = 100; b <= 999; b++) {
 
+        //Gets product of multipliers and converts them to string
         product = a * b;
         checkPalindrome = (product + "");
+
         //Check if number is palindrome
-        if ((checkPalindrome.charAt(0) == checkPalindrome.charAt(checkPalindrome.length-1)) && (checkPalindrome.charAt(1) == checkPalindrome.charAt(checkPalindrome.length-2)) && (checkPalindrome.charAt(2) == checkPalindrome.charAt(checkPalindrome.length-3))) {
+        if ((checkPalindrome.charAt(0) == checkPalindrome.charAt(checkPalindrome.length - 1)) && (checkPalindrome.charAt(1) == checkPalindrome.charAt(checkPalindrome.length - 2)) && (checkPalindrome.charAt(2) == checkPalindrome.charAt(checkPalindrome.length - 3))) {
 
             //Checks if product is the biggest palindrome so far
             if (product > greatest) {

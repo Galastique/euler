b0d76044b55c6804d651fa46fdc84b06d290efa0
Galastique <anthokess@hotmail.com>
2021-11-09 13:18:26 -0500

    Add files via upload

---
+++ javascript/problem004.js
@@ -0,0 +1,26 @@
+//Variables
+var product = 0;
+var greatest = 0;
+
+
+//Checks first multiplier
+for (a = 100; a <= 999; a++) {
+
+    //Checks second multiplier
+    for (b = 100; b <= 999; b++) {
+
+        product = a * b;
+        checkPalindrome = (product + "");
+        //Check if number is palindrome
+        if ((checkPalindrome.charAt(0) == checkPalindrome.charAt(checkPalindrome.length-1)) && (checkPalindrome.charAt(1) == checkPalindrome.charAt(checkPalindrome.length-2)) && (checkPalindrome.charAt(2) == checkPalindrome.charAt(checkPalindrome.length-3))) {
+
+            //Checks if product is the biggest palindrome so far
+            if (product > greatest) {
+                greatest = product;
+            }
+        }
+    }
+}
+
+//Displays answer
+console.log(greatest); //Expected 906609

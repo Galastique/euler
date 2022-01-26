b1ee97030fa1227ea878f52965b667f72a9fe5d5
Galastique <anthokess@hotmail.com>
2022-01-26 04:08:23 -0500

    Add files via upload

---
+++ python/problem019.py
@@ -0,0 +1,21 @@
+import datetime
+
+#Variable
+sundays = 0
+
+
+#For every year
+for year in range(1901,2001):
+    #For every month
+    for month in range(1,13):
+        #Get weekday of first day of the month
+        day = datetime.date(year, month, 1)
+        weekday = day.weekday()
+
+        #If day 1 = sunday
+        if weekday == 6:
+            sundays += 1
+
+
+#Displays answers
+print(sundays) #Expected 171

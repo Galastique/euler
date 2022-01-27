8d484a1b20a640707711e93dba84af4908a1c56d
Galastique <anthokess@hotmail.com>
2022-01-27 13:33:22 -0500

    Add files via upload

---
+++ javascript/problem012.js
@@ -0,0 +1,23 @@
+//Variables
+let divisors = 0;
+let number = 0;
+
+//Finds triangular number
+for(x = 1; x < 100000; x++){
+    divisors = 0;
+    number += x;
+
+    //Finds new divisors
+    for(y = 1; y < Math.round(number/2)+1; y++){
+        if(number % y == 0){
+            divisors += 1;
+        }
+    }
+
+    //Checks if answer has been found
+    if(divisors > 500){
+        //Displays answers
+        console.log(number); //Expected 76576500
+        process.exit(1);
+    }
+}

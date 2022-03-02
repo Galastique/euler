f47cbaa63b80326633e9d42ba6c4de3cd829ff54
Galastique <anthokess@hotmail.com>
2022-03-02 12:08:09 -0500

    Delete problem024_2.py

--- problem024_2.py
+++
@@ -1,7 +0,0 @@
-from itertools import permutations
-
-#Finds million's permutation
-answer = "".join(list(permutations("0123456789"))[999999])
-
-#Displays answer
-print(answer) #Expected 2783915460

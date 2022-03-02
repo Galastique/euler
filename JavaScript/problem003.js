cde1355ee3c88f23841c23506a628b5202613cc8
Galastique <anthokess@hotmail.com>
2022-03-02 12:21:23 -0500

    Add files via upload

--- JavaScript/problem003.js
+++ JavaScript/problem003.js
@@ -3,7 +3,7 @@ let factors = 1;
 let number = 600851475143;
 
 //Finds prime factors of 600851475143
-for (let prime = 1; prime < number; prime++) {
+for (let prime = 1; prime < Math.floor(number/2); prime++) {
 
     //Multiplies number to total if its prime
     if (number % prime == 0) {
